56d2998e92240fecd1a9ada9fd60dee1063d836b
Jayanaath S <jayanaath.srirang004@gmail.com>
2026-05-30 16:15:47 +0530

    Removed unused import in java file

--- backend/src/main/java/com/techstore/backend/services/UserService.java
+++ backend/src/main/java/com/techstore/backend/services/UserService.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.techstore.backend.models.User;
 import com.techstore.backend.config.CustomUserDetails;
